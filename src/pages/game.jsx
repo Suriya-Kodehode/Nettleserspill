1f9353d884013bae0d3f11964b0e990acde6f085
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-03-31 12:04:21 +0200

    update router

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -5,7 +5,8 @@ function Game() {
     return (
         <>
             <div>
-
+                <h2>Example page</h2>
+                <p>this is where the game will be</p>
             </div>
         </>
     )
--- src/pages/start.jsx
+++ src/pages/start.jsx
@@ -1,11 +1,17 @@
 import styles from '../CSSModules/start.module.css'
 
+import { Link } from 'react-router-dom';
+
 function Start() {
 
     return (
         <>
             <div>
                 <h1>Tower Defense</h1>
+
+                <Link to="/game">
+                    <button>Example link to game page</button>
+                </Link>
             </div>
         </>
     )
