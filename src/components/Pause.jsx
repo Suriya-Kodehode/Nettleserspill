634c2424560c34893b4f2de2e091885b3bb536b4
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-04-28 10:50:15 +0200

    update pause button

--- src/components/Pause.jsx
+++ src/components/Pause.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import styles from "../CSSModules/pause.module.css";
 
-function Break() {
-  console.log("pause");
-}
-
 export default function Pause() {
+  const [isPaused, setIsPaused] = useState(true);
+
+  const toggleState = () => {
+    if (isPaused) {
+      console.log("playing");
+    } else {
+      console.log("paused");
+    }
+    setIsPaused(!isPaused);
+  };
+
   return (
     <div className={styles.Bscreen}>
-      <button className={styles.pause} onClick={Break}>
-        <p>||</p>
+      <button className={styles.pause} onClick={toggleState}>
+        {isPaused ? (
+          <p>||</p>
+        ) : (
+          <img src="/images/icons/play button.svg" alt="Play button" />
+        )}
       </button>
     </div>
   );
