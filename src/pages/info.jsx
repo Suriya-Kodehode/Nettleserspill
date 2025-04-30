da1a458f8c07ba264de7a45e233e6b63bc3d3ef3
Tor-HodeKode <tor.kodehode@gmail.com>
2025-04-30 13:54:28 +0200

    Update info.jsx

--- src/pages/info.jsx
+++ src/pages/info.jsx
@@ -31,10 +31,17 @@ function Info() {
             description: 'Hjertet gir deg ekstra liv.',
             health: 'Helse: 30',
             speed: 'Hastighet: Høy'
+<<<<<<< Updated upstream
         },
         {
             name: 'Boss',
             image: '/images/Boss.png', // Legg til riktig bildebane
+=======
+        }
+        {
+            name: 'Cannon',
+            image: '/images/Cannon.gif', // Legg til riktig bildebane
+>>>>>>> Stashed changes
             description: 'En kraftig kanon som skyter ett flammende inferno, men med høy skade.',
             damage: 'Skade: 120',
             range: 'Rekkevidde: 80'
