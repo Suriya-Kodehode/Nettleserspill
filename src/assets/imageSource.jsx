e30644d4b17046af080de8454b67f04a65e60569
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-05-05 11:38:09 +0200

    Update imageSource.jsx

--- src/assets/imageSource.jsx
+++ src/assets/imageSource.jsx
@@ -19,12 +19,25 @@ const paths = (folderKey, fileName) => {
 
 export const enemyImages = {
     monkey: paths("enemy", "MonkeyAni.gif"),
+    boss: paths("enemy", "Boss.png"),
+    cat: paths("enemy", "cat.gif"),
 };
 
 export const mapImages = {
     newDawn: paths("maps", "New%20Dawn.png"),
+    bigMap: paths("maps", "BigMap.png"),
+    dirt: paths("maps", "Dirt.png"),
+    easyMap: paths("maps", "EasyMap.png"),
+    map: paths("maps", "map.png"),
+    ThePath: paths("maps", "ThePath.png"),
 }
 
 export const iconImages = {
     heart: paths("icons", "mdi_heart.svg"),
+    playButton: paths("icons", "play%20button.svg"),
+    redBalloon: paths("icons", "Red%20balloon.svg"),
+}
+
+export const towerImages = {
+    balloonGunner: paths("tower", "BalloonGunner.gif"),
 }
